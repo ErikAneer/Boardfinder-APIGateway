e118946923dd5f3af0723af03c4d1be148c77080
ErikAneer <erik.aneer@yh.nackademin.se>
2019-12-04 11:09:01 +0100

    Amending code for service-registry

--- src/main/java/Boardfinder/APIgateway/ApiGatewayApplication.java
+++ src/main/java/Boardfinder/APIgateway/ApiGatewayApplication.java
@@ -1,18 +1,12 @@
 package Boardfinder.APIgateway;
 
-import com.netflix.ribbon.proxy.annotation.Http.HttpMethod;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.stream.Collectors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
