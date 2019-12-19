85161e2e06afd06ffe4972841a7a06298b6f2fcf
ErikAneer <erik.aneer@yh.nackademin.se>
2019-12-19 21:41:59 +0100

    Modifications to send apporpriate statistics data/objects to client.

--- src/main/java/Boardfinder/APIgateway/ApiGatewayApplication.java
+++ src/main/java/Boardfinder/APIgateway/ApiGatewayApplication.java
@@ -2,12 +2,20 @@ package Boardfinder.APIgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
+
